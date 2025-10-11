b2d0b0bd020c509bb056c38548be088a4172dd41
zh1peng <frueddy@gmail.com>
2025-10-11 16:31:33 +0800

    update example folder & add load mff for egi

--- +prep/load_set.m
+++ +prep/load_set.m
@@ -2,7 +2,7 @@ function [EEG, out] = load_set(~, varargin)
 % LOAD_SET  Load an EEGLAB .set dataset
 %
 % Usage:
-%   [EEG, out] = eegdojo.prep.load_set([], 'filename','x.set','filepath','./data')
+%   [EEG, out] = prep.load_set([], 'filename','x.set','filepath','./data')
 %
     p = inputParser; 
     p.addParameter('filename','',@ischar);
@@ -17,6 +17,5 @@ function [EEG, out] = load_set(~, varargin)
     EEG = eeg_checkset(EEG);
     out = struct('loadedFile', fullfile(R.filepath, R.filename));
     logPrint(R.LogFile, sprintf('[load_set] Dataset loaded %s/%s', R.filepath, R.filename)); % Added [load_set] prefix
-
 
 end
